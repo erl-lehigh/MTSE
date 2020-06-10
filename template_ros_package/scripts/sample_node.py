f90e99c2a6741fe5fc94094d2daa42ab3b2b8c21
Yubo Wang <yuw220@lehigh.com>
2020-06-10 15:28:14 -0400

    Finalized the ros_package except costmap

--- template_ros_package/scripts/sample_node.py
+++ template_ros_package/scripts/sample_node.py
@@ -2,9 +2,10 @@
 
 import rospy
 
-rospy.init_node('THE_NAME_OF_THE_NODE')
-rate = rospy.Rate(1)
+if __name__ == "__main__":
+    rospy.init_node('THE_NAME_OF_THE_NODE')
+    rate = rospy.Rate(1)
 
-while not rospy.is_shutdown():
-    print("Hello World")
-    rate.sleep()
+    while not rospy.is_shutdown():
+        print("Hello World")
+        rate.sleep()
