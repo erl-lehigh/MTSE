6ef76f57e29594779f972f60338ed13089e39472
vaa222 <vaa222@lehigh.edu>
2020-06-26 14:15:37 -0400

    purepursuit ROS

---
+++ purepursuit/scripts/purepursuit_node.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+
+import rospy
+from nav_msgs.msg import Path
+from ackermann_msgs.msg import AckermannDrive
+
+from purepursuit import PurePursuit
+
+
+
+class PUREPURSUITROSNode(object):
+    '''TODO:
+    '''
+
+    def __init__(self):
+        '''TODO:
+        '''
+        self.node_name = rospy.get_name()
+
+        # Read parameters
+        #TODO: read parameters
+        # second parameter is default value
+        self.example = rospy.get_param('~parameter', 0)
+
+        # Create publishers
+        # TODO:
+        # second parameter is the message type for the topic
+        self.example_pub  = rospy.Publisher('speed_command', AckermannDrive,
+                                            queue_size=1)
+
+        # Create subscribers
+        self.example_sub = rospy.Subscriber('~subscriber_example', Path,
+                                            self.example_callback)
+
+        rospy.loginfo('[%s] Node started!', self.node_name)
+
+    def example_callback(self, msg):
+        '''Example message callback.'''
+        print('Message received.')
+
+
+if __name__ == "__main__":
+    # Initialize node with rospy
+    rospy.init_node('purepursuit', anonymous=False)
+    # Create the node object
+    _ = PUREPURSUITROSNode()
+    # Keep the node alive
+    rospy.spin()
