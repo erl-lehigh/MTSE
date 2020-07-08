625c0dd9d62341e8d9a78ef436062516edca18eb
Nathan Bowler <nathan104022@gmail.com>
2020-07-08 10:51:51 -0400

    an attempt at getting the osmnx code to work in python

--- navigation/route_planner/scripts/route_planner_node.py
+++ navigation/route_planner/scripts/route_planner_node.py
@@ -2,6 +2,10 @@
 
 import rospy
 from nav_msgs.msg import Path
+import matplotlib.pyplot as plt
+import Rtree==0.8.3
+import osmnx==0.9 as ox
+import networkx as nx
 
 from route_planner import RoutePlanner
 
@@ -34,7 +38,15 @@ class RoutePlannerNode(object):
 
     def example_callback(self, msg):
         '''Example message callback.'''
-        print('Message received.')
+        #print('Message received.')
+        g = ox.graph_from_address('19 Memorial Dr W, Bethlehem, Pennsylvania', distance=1000, network_type='drive')
+        orig = (-75.3868, 40.6116)
+        dest = (-75.3684, 40.6039)
+        origin_node = ox.get_nearest_node(g, orig)
+        destination_node = ox.get_nearest_node(g, dest)
+        route = nx.shortest_path(g, origin_node, destination_node)
+        fig, ax = ox.plot_graph_route(g, route, route_linewidth=6, node_size=0, bgcolor='k')
+        ox.plot_graph(g)
 
 
 if __name__ == "__main__":
