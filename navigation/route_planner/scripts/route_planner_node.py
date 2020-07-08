b6034d4c63500bdf7b31aad424cd23165ea561b2
Nathan Bowler <nathan104022@gmail.com>
2020-07-08 11:18:46 -0400

    tried to implement the node using ros, don't know how to test it

--- navigation/route_planner/scripts/route_planner_node.py
+++ navigation/route_planner/scripts/route_planner_node.py
@@ -5,7 +5,6 @@ from nav_msgs.msg import Path
 import matplotlib.pyplot as plt
 #import Rtree==0.8.3
 #import osmnx==0.9 as ox
-import osmnx as ox
 import networkx as nx
 
 #from route_planner import RoutePlanner
@@ -28,13 +27,25 @@ class RoutePlannerNode(object):
         # Create publishers
         # TODO:
         # second parameter is the message type for the topic
-        self.example_pub  = rospy.Publisher('publisher_example', Path,
-                                            queue_size=1)
+        '''self.example_pub  = rospy.Publisher('publisher_example', Path,
+                                            queue_size=1)'''
+        curr_location = (40.6119486, -75.3785700)
+        self.example_pub = rospy.Publisher('', float64, queue_size=10)
+        rospy.init_node('location', anonymous=True)
+        rate = rospy.Rate(0.25) # 0.25hz = 4s
+        msg = curr_location
+        while not rospy.is_shutdown():
+            rospy.loginfo('current location x: %f, y: %f', msg[0], msg[1])
+            pub.publish(msg)
+            rate.sleep()
 
         # Create subscribers
-        self.example_sub = rospy.Subscriber('~subscriber_example', Path,
-                                            self.example_callback)
-
+        '''self.example_sub = rospy.Subscriber('~subscriber_example', Path,
+                                            self.example_callback)'''
+        rospy.init_node('location', anonymous=True)
+        rospy.Subscriber('', float64, example_callback(self, msg))
+        # spin() simply keeps python from exiting until this node is stopped
+        rospy.spin()
         rospy.loginfo('[%s] Node started!', self.node_name)
 
     def example_callback(self, msg):
@@ -42,7 +53,7 @@ class RoutePlannerNode(object):
         #print('Message received.')
         center_point = (40.6038914, -75.3739361)
         g = ox.graph_from_point(center_point, distance=1500, network_type='drive')
-        orig = (40.6119486, -75.3785700)  #orig = input(‘input coordinate: ’)
+        orig = (40.6119486, -75.3785700)  
         dest = (40.6054017, -75.3758301)
         origin_node = ox.get_nearest_node(g, orig)
         destination_node = ox.get_nearest_node(g, dest)
