bc1d563c690a19d775f83cec1f9bb046abbaf491
Nathan Bowler <nathan104022@gmail.com>
2020-07-08 10:56:14 -0400

    switched to mountaintop version

--- navigation/route_planner/scripts/route_planner_node.py
+++ navigation/route_planner/scripts/route_planner_node.py
@@ -3,11 +3,11 @@
 import rospy
 from nav_msgs.msg import Path
 import matplotlib.pyplot as plt
-import Rtree==0.8.3
-import osmnx==0.9 as ox
+#import Rtree==0.8.3
+#import osmnx==0.9 as ox
 import networkx as nx
 
-from route_planner import RoutePlanner
+#from route_planner import RoutePlanner
 
 #RoutePlanner
 class RoutePlannerNode(object):
@@ -39,14 +39,16 @@ class RoutePlannerNode(object):
     def example_callback(self, msg):
         '''Example message callback.'''
         #print('Message received.')
-        g = ox.graph_from_address('19 Memorial Dr W, Bethlehem, Pennsylvania', distance=1000, network_type='drive')
-        orig = (-75.3868, 40.6116)
-        dest = (-75.3684, 40.6039)
+        center_point = (40.6038914, -75.3739361)
+        g = ox.graph_from_point(center_point, distance=1500, network_type='drive')
+        orig = (40.6119486, -75.3785700)  #orig = input(‘input coordinate: ’)
+        dest = (40.6054017, -75.3758301)
         origin_node = ox.get_nearest_node(g, orig)
         destination_node = ox.get_nearest_node(g, dest)
         route = nx.shortest_path(g, origin_node, destination_node)
-        fig, ax = ox.plot_graph_route(g, route, route_linewidth=6, node_size=0, bgcolor='k')
+        fig, ax = ox.plot.plot_graph_route(g, route, route_linewidth=6, node_size=0, bgcolor='k')
         ox.plot_graph(g)
+
 
 
 if __name__ == "__main__":
