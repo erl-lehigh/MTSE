ad10c732aba80241afb87a0203a831c685c9d459
Nathan Bowler <nathan104022@gmail.com>
2020-07-13 18:23:23 -0400

    found the geometric path

--- navigation/route_planner/scripts/route_planner_node.py
+++ navigation/route_planner/scripts/route_planner_node.py
@@ -13,6 +13,7 @@ from route_planner import RoutePlanner
 from std_msgs.msg import *
 from nav_msgs.msg import Path
 from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
+
 #RoutePlanner
 class RoutePlannerNode(object):
     '''TODO:
@@ -22,6 +23,12 @@ class RoutePlannerNode(object):
     [258574407, 258574525, 259255502]
     [INFO] [1594667836.981949]: current location x: 40.608300, y: -75.374300
     [258574401, 259349128, 258574406, 258574407, 258574525, 259255502]
+
+    print(g.get_edge_data(route[0],route[1])) #THIS LINE GIVES:
+    {0: {'length': 135.28, 'osmid': 31977833, 'oneway': False, 'geometry': <shapely.geometry.linestring.LineString object at 0x7f1c7d853e90>, 'highway': u'residential', 'name': u'Taylor Street'}}
+    [INFO] [1594677784.641976]: current location x: -75.381000, y: 40.607900
+    {0: {'length': 127.473, 'osmid': 30956376, 'oneway': False, 'geometry': <shapely.geometry.linestring.LineString object at 0x7f1c7c238490>, 'highway': u'residential', 'name': u'West Packer Avenue'}}
+
     '''
     def printGraph(self, msg):
         #The center point of the map
@@ -38,13 +45,14 @@ class RoutePlannerNode(object):
         destination_node = ox.get_nearest_node(g, dest)
         #Calculates the shortest path from the current location to the destination
         route = nx.shortest_path(g, origin_node, destination_node)
+        print list(g.get_edge_data(route[0],route[1])[0]["geometry"].coords)
         #print(g.node[route[0]]) #Turns node IDs into Coordinates
         #Graphs the figure
         fig, ax = ox.plot.plot_graph_route(g, route, route_linewidth=6, node_size=0, bgcolor='k')
 
         msg2 = Path()
         msg2.header.frame_id = "/map"
-        #msg2.header.stamp =  rospy.rostime.time
+        msg2.header.stamp =  rospy.Time.now()
         for node in route:
             x = g.node[node]["x"]
             y = g.node[node]["y"]
@@ -82,23 +90,20 @@ if __name__ == "__main__":
     #Initializes the msg as a Float64MultiArray
     msg = Float64MultiArray()
     #Sets how often the messages are sent
-    rate = rospy.Rate(0.2) # 0.2hz
+    rate = rospy.Rate(0.1) # 0.1hz
     #Initializes the msg to a generic location
     msg.data = [0.0, 0.0]
     
-
-
-
-
     #Control Loop
     while not rospy.is_shutdown():
+        plt.ion()
         #Sets the First Point
         curr_location = (40.6079000,-75.3810000) #(y,x)
         msg.data = curr_location #Updates msg with new location
         rospy.loginfo('current location x: %f, y: %f', msg.data[1], msg.data[0]) #Outputs Debugging information
         node1.currPos_pub.publish(msg) #Publishes the msg
         rate.sleep()  #Waits the sleep time
-        #plt.close(1)
+
 
         #Sets the Second Point
         curr_location = (40.6083000, -75.3743000) #(y,x)
@@ -106,5 +111,4 @@ if __name__ == "__main__":
         rospy.loginfo('current location x: %f, y: %f', msg.data[1], msg.data[0]) #Outputs Debugging information
         node1.currPos_pub.publish(msg) #Publishes the msg
         rate.sleep() #Waits the sleep time
-        #plt.close(1)
 
