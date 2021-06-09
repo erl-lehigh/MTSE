88585186bb09b5235bfc96f713ae166a737098f6
Brian Zhu <blz222@lehigh.edu>
2021-06-09 09:30:43 -0400

    added description

--- navigation/rrt/src/rrt/__init__.py
+++ navigation/rrt/src/rrt/__init__.py
@@ -1,5 +1,6 @@
-'''TODO:
-
+'''
+Uses the RRT algorithm to generate a dubins path
+from a start configuration to a goal configuration
 '''
 from rrt import RRTPlanner
 from dubins import DubinsState, DynamicDubinsVehicle
